57beeaedb07254ce943d5edddd43659f2286da15
tcwcodes <35410637+tcwcodes@users.noreply.github.com>
2018-08-10 19:17:53 -0500

    port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cheerio = require("cheerio");
 
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -20,6 +20,8 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
+
 
 app.get("/scrape", function(req, res) {
     axios.get("https://www.npr.org/sections/news/").then(function(response) {
